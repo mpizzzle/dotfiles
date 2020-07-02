9efc495c8aab5a6bf21163938139cb079620532c
mpizzzle <m@michaelpercival.xyz>
2020-07-02 11:48:45 +0100

    somtimes having everything in spanish is tiring.

--- .local/bin/next.sh
+++ .local/bin/next.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-psql spanish-dictionary -c "SELECT * FROM dictionary WHERE next_appearance < current_timestamp ORDER BY next_appearance" | grep filas
+psql spanish-dictionary -c "SELECT * FROM dictionary WHERE next_appearance < current_timestamp ORDER BY next_appearance" | tail -n 2 | sed q
